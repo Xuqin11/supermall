87d20c5ff78217ee58a2b29ca5302cf869f3de0c
chenxuqin <564769439@qq.com>
2021-03-28 01:20:58 +0800

    保存商品数据，首页数据请求

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,46 +4,105 @@
     <home-swiper :banners="banners"/>
     <recommend-view :recommends="recommends"/>
     <feature-view></feature-view>
+    <tab-control :titles="['流行', '新款', '精选']"></tab-control>
+    <goods-list :goods="goods['pop'].list"></goods-list>
     <ul>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
     </ul>
   </div>
 </template>
 
 <script>
-  import NavBar from 'components/common/navbar/NavBar';
+  import NavBar from 'components/common/navbar/NavBar'
+  import TabControl from 'components/content/tabControl/TabControl'
+  import GoodsList from 'components/content/goods/GoodsList'
+  import GoodsListItem from 'components/content/goods/GoodsListItem'
+
   import HomeSwiper from './childComps/HomeSwiper'
   import RecommendView from './childComps/RecommendView'
   import FeatureView from './childComps/FeatureView'
 
-  import {getHomeMultidata} from "network/home";
+  import {getHomeMultidata, getHomeGoods} from "network/home";
 
   export default {
     name: "Home",
     components: {
       NavBar,
+      TabControl,
+      GoodsList,
+      GoodsListItem,
+
       HomeSwiper,
       RecommendView,
-      FeatureView
+      FeatureView,
     },
     data() {
       return {
         banners: [],
-        recommends: []
+        recommends: [],
+        goods: {
+          'pop': {page: 0, list: []},
+          'new': {page: 0, list: []},
+          'sell': {page: 0, list: []}
+        }
       }
     },
     created() {
-      // 1.请求多个数据
-      getHomeMultidata().then(res => {
-        // this.result = res;
+      this.getHomeMultidata();
+
+      this.getHomeGoods('pop');
+      this.getHomeGoods('new');
+      this.getHomeGoods('sell');
+    },
+    methods: {
+      // 请求轮播图的数据
+      getHomeMultidata() {
+        getHomeMultidata().then(res => {
         this.banners = res.data.banner.list;
         this.recommends = res.data.recommend.list;
       })
+      },
+      // 请求商品信息的数据
+      getHomeGoods(type) {
+        const page = this.goods[type].page + 1;
+        getHomeGoods(type, page).then(res => {
+        // console.log(res);
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page += 1
+      }).catch(err => {
+        console.log(err);
+      })
+      }
     }
   }
 </script>
