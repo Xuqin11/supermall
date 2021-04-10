f605713006f7d44aab8fcf19e0279159e0f320ba
chenxuqin <564769439@qq.com>
2021-04-11 00:33:02 +0800

    详情页轮播图 商品基本信息展示 店铺信息展示 加入滚动效果scroll

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,28 +1,76 @@
 <template>
   <div id="Detail">
-    <detail-nav-bar></detail-nav-bar>
+    <detail-nav-bar class="detail-nav"></detail-nav-bar>
+    <scroll class="content">
+      <detail-swiper :top-images="topImages"></detail-swiper>
+      <detail-base-info :goods="goods"></detail-base-info>
+      <detail-shop-info :shop="shop"></detail-shop-info>
+    </scroll>
   </div>
 </template>
 
 <script>
 import DetailNavBar from './childComp/DetailNavBar'
+import DetailSwiper from './childComp/DetailSwiper'
+import DetailBaseInfo from './childComp/DetailBaseInfo'
+import DetailShopInfo from './childComp/DetailShopInfo'
+import Scroll from 'components/common/scroll/Scroll'
+
+import {getDetail, Goods, Shop} from 'network/detail.js'
 
 export default {
   name: 'Detail',
   components: {
-    DetailNavBar
+    DetailNavBar,
+    DetailSwiper,
+    DetailBaseInfo,
+    DetailShopInfo,
+    Scroll
   },
   data() {
     return {
-      iid: null
+      iid: null,
+      topImages: [],
+      goods: {},
+      shop: {}
     }
   },
-  activated() {
+  created() {
+    // 1.保存传入的iid
     this.iid = this.$route.params.iid
+
+    // 2.根据iid请求详细数据
+    getDetail(this.iid).then(res => {
+      // console.log(res);
+      // 1. 获取轮播图数据
+      const data = res.result
+      this.topImages = data.itemInfo.topImages
+
+      // 2. 获取商品信息
+      this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+
+      // 3. 创建店铺信息
+      this.shop = new Shop(data.shopInfo)
+    })
   }
 }
 </script>
 
 <style  scoped>
+#Detail {
+  position: relative;
+  height: 100vh;
+  z-index: 9;
+  background-color: #fff;
+}
 
+.detail-nav {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+}
+
+.content {
+  height: calc(100% - 44px);
+}
 </style>
